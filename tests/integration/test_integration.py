41381f6e29ab46795d956d7fad26140dde29fd6c
guillaume <guillaume.belanger27@gmail.com>
2024-03-05 18:24:34 -0500

    feat: implement log forwarding

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -13,6 +13,16 @@ logger = logging.getLogger(__name__)
 
 METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
 APPLICATION_NAME = METADATA["name"]
+GRAFANA_AGENT_CHARM_NAME = "grafana-agent-k8s"
+
+
+async def deploy_grafana_agent(ops_test):
+    """Deploy the grafana-agent charm."""
+    await ops_test.model.deploy(
+        GRAFANA_AGENT_CHARM_NAME,
+        application_name=APPLICATION_NAME,
+        channel="stable",
+    )
 
 
 @pytest.fixture(scope="module")
@@ -36,6 +46,23 @@ async def test_given_charm_is_built_when_deployed_then_status_is_active(
     ops_test,
     build_and_deploy,
 ):
+    await ops_test.model.wait_for_idle(
+        apps=[APPLICATION_NAME],
+        status="active",
+        timeout=1000,
+    )
+
+
+@pytest.mark.abort_on_fail
+async def test_given_grafana_agent_is_deployed_when_relation_is_made_then_status_is_active(
+    ops_test,
+    build_and_deploy,
+):
+    await deploy_grafana_agent(ops_test)
+    await ops_test.model.integrate(
+        relation1=f"{APPLICATION_NAME}:logging",
+        relation2=GRAFANA_AGENT_CHARM_NAME,
+    )
     await ops_test.model.wait_for_idle(
         apps=[APPLICATION_NAME],
         status="active",
