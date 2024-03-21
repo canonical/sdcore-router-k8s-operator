1be8dba8779526fdc58cff19e72f109f262be4c2
Dario Faccin <35623244+dariofaccin@users.noreply.github.com>
2024-03-21 09:22:24 +0100

    chore: handle charm status with ops CollectStatus event (#46)

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -26,7 +26,6 @@ async def deploy_grafana_agent(ops_test):
 
 
 @pytest.fixture(scope="module")
-@pytest.mark.abort_on_fail
 async def build_and_deploy(ops_test):
     """Build the charm-under-test and deploy it."""
     charm = await ops_test.build_charm(".")
