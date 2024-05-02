8d5c35039c8190d498ad67761a0e6d02879af10a
Guillaume Belanger <guillaume.belanger27@gmail.com>
2024-05-02 11:05:00 -0400

    chore: centralise metadata in charmcraft.yaml

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -12,7 +12,7 @@ from pytest_operator.plugin import OpsTest
 
 logger = logging.getLogger(__name__)
 
-METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
+METADATA = yaml.safe_load(Path("./charmcraft.yaml").read_text())
 APPLICATION_NAME = METADATA["name"]
 GRAFANA_AGENT_CHARM_NAME = "grafana-agent-k8s"
 GRAFANA_AGENT_CHARM_CHANNEL = "latest/stable"
